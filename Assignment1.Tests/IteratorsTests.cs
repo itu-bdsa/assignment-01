580a6b9014f8e969a6038bd9d1ef05391c2c7fdb
ClaraWJ <clwj@itu.dk>
2022-09-09 14:28:44 +0200

    Generics answer and Iterators Flatten implemented

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,24 @@
-namespace Assignment1.Tests;
+using Xunit; 
+using FluentAssertions; 
+namespace Assignment1.Tests;    
+
 
 public class IteratorsTests
 {
+    [Fact]
+    public void Should_Return_A_Stream_Of_Ts()
+    {
+        // Arrange
+            var listOfLists = new List<List<int>>(); 
+            listOfLists.Add(new List<int> {1, 2, 3, 4, 5});
+            listOfLists.Add(new List<int> {6, 7, 8, 9, 10});
+    
+        // Act
+            var result = Iterators.Flatten(listOfLists); 
+    
+        // Assert
+            //result.Should().BeEquivalentTo(new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            Assert.Equal(result, new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});  
+
+    }
 }
---
+++ Assignment1.Tests/Usings.cs
@@ -0,0 +1,4 @@
+global using Xunit;
+global using FluentAssertions;
+global using System.Reflection;
+global using System; 
