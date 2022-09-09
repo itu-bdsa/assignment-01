583d317b55575a00c9c9f55207e10f7c62accd3d
Magnus Brun Larsen <magnusbrunlarsen00@gmail.com>
2022-09-09 14:08:26 +0200

    Make test and implement flatten method

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -2,4 +2,20 @@ namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void flatten() 
+    {
+        // Arrange
+        List<List<int>> listInList = new List<List<int>>();
+        listInList.Add(new List<int>{1,2,3});
+        listInList.Add(new List<int>{4,5,6});
+        listInList.Add(new List<int>{7,8,9});
+
+        // Act 
+        var result = Iterators.Flatten<int>(listInList);
+
+        // Assert
+        result.Should().BeEquivalentTo(new List<int> {1,2,3,4,5,6,7,8,9});
+    }
+    
 }
