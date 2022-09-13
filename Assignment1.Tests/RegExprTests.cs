415c7d80a618eeeac267ae3b6eb89f82eb791502
Rasmus Nielsen <rasni@itu.dk>
2022-09-13 20:21:11 +0200

    Resolutions

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -42,4 +42,52 @@ public class RegExprTests
         // Assert
         actual.Should().Equal("Hello", "World", "Hello", "Test");
     }
+
+    [Fact]
+    public void Resolution_Empty()
+    {
+        // Arrange
+        var empty = "";
+
+        // Act
+        var actual = Assignment1.RegExpr.Resolution(empty);
+
+        // Assert
+        actual.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Resolution_Single()
+    {
+        // Arrange
+        var single = "1920x1080";
+
+        // Act
+        var actual = Assignment1.RegExpr.Resolution(single);
+
+        // Assert
+        actual.Should().Equal((1920, 1080));
+    }
+
+    [Fact]
+    public void Resolution_Multiple()
+    {
+        // Arrange
+        var multiple = "1920x1080, 1024x768, 800x600, 640x480, 320x200, 320x240, 800x600, 1280x960";
+
+        // Act
+        var actual = Assignment1.RegExpr.Resolution(multiple);
+
+        // Assert
+        actual.Should().Equal(
+            (1920, 1080),
+            (1024, 768),
+            (800, 600),
+            (640, 480),
+            (320, 200),
+            (320, 240),
+            (800, 600),
+            (1280, 960));
+    }
+
 }
