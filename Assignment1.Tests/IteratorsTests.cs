76211a4bdc570c0828cb58c12066c8c13a121eb4
emno <emno@itu.dk>
2022-09-13 12:39:32 +0200

    make initial tests that fail
    This will setup the project, make global and
    local usings and so fourth.

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -2,4 +2,28 @@ namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+
+    [Fact]
+    public void FilterAllTrueNaturalNumbers(){
+        //Setup
+        Predicate<int> all = All;
+        bool All(int i) => true;
+        var input = new List<int>();
+        for(int i = 0; i < 5; i++){
+            input.Add(i);
+        }
+
+        //Act
+        var output = Iterators.Filter<int>(input, all);
+
+        //Assert
+        var count = 0;
+        foreach(int value in output){
+            count++;
+        }
+
+        Assert.Equal(count, 5);
+
+    }
+
 }
