d0d02cdc2901ff3815b121bd99e6e77f9e9c3a08
emno <emno@itu.dk>
2022-09-13 13:03:09 +0200

    implement Filter and add tests

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -22,8 +22,69 @@ public class IteratorsTests
             count++;
         }
 
-        Assert.Equal(count, 5);
-
+        Assert.Equal(5, count);
     }
+
+    [Fact]
+    public void FilterAllTrueNaturalNumbersStartingHigh(){
+        //Setup
+        Predicate<int> all = All;
+        bool All(int i) => true;
+        var input = new List<int>();
+        for(int i = 100; i < 105; i++){
+            input.Add(i);
+        }
+
+        //Act
+        var output = Iterators.Filter<int>(input, all);
+
+        //Assert
+        var count = 0;
+        foreach(int value in output){
+            count++;
+        }
+        Assert.Equal(5, count);
+    }
+
+    [Fact]
+    public void FilterEvensOnly(){
+        //Setup
+        Predicate<int> even = Even;
+        bool Even(int i) => i % 2 == 0;
+        var input = new List<int>();
+        for(int i = 0; i < 10; i++){
+            input.Add(i);
+        }
+
+        //Act
+        var output = Iterators.Filter<int>(input, even);
+
+        //Assert
+        var count = 0;
+        foreach(int value in output){
+            count++;
+        }
+        Assert.NotEqual(output, input);
+        Assert.Equal(5, count);
+    }
+
+    [Fact]
+    public void FilterEmpty(){
+        //Setup
+        Predicate<int> even = Even;
+        bool Even(int i) => i % 2 == 0;
+        var input = new List<int>();
+
+        //Act
+        var output = Iterators.Filter<int>(input, even);
+
+        //Assert
+        var count = 0;
+        foreach(int value in output){
+            count++;
+        }
+        Assert.Equal(0, count);
+    }
+
 
 }
