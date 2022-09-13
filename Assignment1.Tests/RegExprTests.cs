b42e3af96d433009b8f7776e86a2a1590249ea4d
tbru <tbru@itu.dk>
2022-09-13 16:54:02 +0200

    add regExpr

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -2,4 +2,97 @@ namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+    [Fact]
+    public void SplitLine_given_something_returns_something()
+    {
+        // Arrange
+        string[] stream = {
+            "this is my line1",
+            "dette er min linje2",
+            "das ist meine Zeile3"
+        };
+
+        // Act
+
+        // Assert
+        var output = RegExpr.SplitLine(stream);
+        var expected = new string[]{
+            "this", "is", "my", "line1",
+            "dette", "er", "min", "linje2",
+            "das", "ist", "meine", "Zeile3"
+        };
+        Assert.Equal(expected, output);
+    }
+
+    [Fact]
+    public void Resolutions_given_sample_input()
+    {
+        // Arrange
+        string[] stream = {
+            "1920x1080",
+            "1024x768, 800x600, 640x480",
+            "320x200, 320x240, 800x600",
+            "1280x960"
+        };
+
+        // Act
+
+        // Assert
+        var output = RegExpr.Resolution(stream);
+        var expected = new[] {
+            (1920, 1080),
+            (1024, 768),
+            (800, 600),
+            (640, 480),
+            (320, 200),
+            (320, 240),
+            (800, 600),
+            (1280, 960),
+        };
+        Assert.Equal(expected, output);
+    }
+
+    [Fact]
+    public void InnerText_given_sample_input()
+    {
+        // Arrange
+        string input = "<div><p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+
+        // Act
+        var output = RegExpr.InnerText(input, "a");
+
+        //Assert
+        string[] expected = {
+            "theoretical computer science",
+            "formal language",
+            "characters",
+            "pattern",
+            "string searching algorithms",
+            "strings"
+        };
+
+        Assert.Equal(expected, output);
+    }
+
+    [Fact]
+    public void Urls_given_sample_input()
+    {
+        // Arrange
+        string input = "<div><p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+
+        // Act
+        var output = RegExpr.Urls(input);
+
+        //Assert
+        (Uri, string)[] expected = {
+            (new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science"),
+            (new Uri("https://en.wikipedia.org/wiki/Formal_language"),"Formal language"),
+            (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"), "Character (computing)"),
+            (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"), "Pattern matching"),
+            (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"),
+            (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")
+        };
+
+        Assert.Equal(expected, output);
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,10 +1,66 @@
 namespace Assignment1;
 
+using System.Text.RegularExpressions;
+
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
+        var pattern = @"[(a-z)(A-Z)0-9]+";
+        foreach(string line in lines)
+        {
+            foreach(Match match in Regex.Matches(line, pattern))
+            {
+                yield return match.Value;
+            }
+        }
+    }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions)
+    {
+        var pattern = @"(?'res'[0-9]+)(?:x)(?'res2'[0-9]+)";
+        foreach(string resolution in resolutions)
+        {
+            foreach(Match match in Regex.Matches(resolution, pattern))
+            {
+                int width, height;
+                Int32.TryParse(match.Groups["res"].Value, out width);
+                Int32.TryParse(match.Groups["res2"].Value, out height);
+                yield return (width, height);
+            }
+        }
+    }
 
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+    public static IEnumerable<string> InnerText(string html, string tag)
+    {
+        var pattern = @"(?<=<([" + tag + @"]+).*?>)(?'innertext'[\p{L} </().,]+)(?=</\1>)";
+        foreach(Match match in Regex.Matches(html, pattern))
+        {
+            yield return match.Groups["innertext"].Value;
+        }
+    }
+
+    public static IEnumerable<(Uri url, string title)> Urls(string html)
+    {
+        var patternInsideTag = @"<([a])(?'inn'.*?)>(.*?)</\1>";
+        foreach(Match match in Regex.Matches(html, patternInsideTag))
+        {
+            string inn = match.Groups["inn"].Value;
+            
+            if(inn.Contains("title"))
+            {
+                var patternUrl = @"https?://[\p{L}.-/_()]+";
+                var patternTitle = @"(?<=title=.).*(?=.)";
+                string url = Regex.Match(inn, patternUrl).Value;
+                string title = Regex.Match(inn, patternTitle).Value;
+                yield return (new Uri(url), title);
+            }
+            else
+            {
+                var patternUrl = @"https?://[\p{L}.-/_()]+";
+                string url = Regex.Match(inn, patternUrl).Value;
+                yield return (new Uri(url),match.Value);
+            }
+        }
+    }
 }
