7e2e27a76fd3b08f76597f8cace6384bf82e4a6d
Marcus Henrik Simonsen <mhsi@itu.dk>
2022-09-13 12:59:13 +0200

    Add test and implementation of Splitline

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -2,4 +2,17 @@ namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+    [Fact]
+    public void Splitline()
+    {
+        // Arrange
+        List<string> list = new List<string> {"Hello world", "Lorem Ipsum", "Hi there Bob", "Bob is 9 years old"};
+
+        // Act
+        var result = RegExpr.SplitLine(list);
+
+        // Assert
+        var expected = new List<string> {"Hello", "world", "Lorem", "Ipsum", "Hi", "there", "Bob", "Bob", "is", "9", "years", "old"};
+        result.Should().BeEquivalentTo(expected);
+    }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,8 +1,20 @@
+using System.Text.RegularExpressions;
+
 namespace Assignment1;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
+        var pattern = @"(\w)+";
+        foreach(string line in lines)
+        {
+            foreach(Match m in Regex.Matches(line, pattern))
+            {
+                yield return m.ToString();
+            }
+        }
+    }
 
     public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
 
