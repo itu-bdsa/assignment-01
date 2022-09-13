e81ab7df6dd8db26b76a295f7b9c7b9eff94f96c
Marcus Henrik Simonsen <mhsi@itu.dk>
2022-09-13 12:34:46 +0200

    Add Filter test and implementation

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -18,4 +18,16 @@ public class IteratorsTests
         result.Should().BeEquivalentTo(new List<int> {1,2,3,4,5,6,7,8,9});
     }
     
+    [Fact]
+    public void filter_even_test()
+    {
+        // Arrange
+        List<int> list = new List<int> {1,2,3,4,5,6,7,8,9};
+
+        // Act 
+        var result = Iterators.Filter<int>(list, (int n) => n % 2 == 0);
+
+        // Assert
+        result.Should().BeEquivalentTo(new List<int> {2,4,6,8});
+    }
 }
