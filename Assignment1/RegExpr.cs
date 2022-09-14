a1207351ce7427e7164307280c22653c36dfe1bc
Rasmus Nielsen <rasni@itu.dk>
2022-09-14 13:38:46 +0200

    Resolution

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -18,7 +18,15 @@ public static class RegExpr
 
     }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
+    public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+    {
+
+        foreach (Match match in Regex.Matches(resolutions, @"(?<width>\d+)x(?<height>\d+)"))
+        {
+            yield return (int.Parse(match.Groups["width"].Value), int.Parse(match.Groups["height"].Value));
+        }
+
+    }
 
     public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
 }
